10c8fd59d8251e3658051b3ee96b6cdf61c158ea
rishigb <rishibhatnagar1@gmail.com>
2017-10-15 16:53:10 +0530

    made changes

--- updateEverything.sh
+++ updateEverything.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Exit if any subcommand fails.
+set -e
 
 echo "running tag generator"
 cd /Users/irgb/Codebase/rishigb.github.io/
@@ -10,3 +12,5 @@ git add -A
 git commit -m"made changes"
 git push -u origin master
 echo "all set, check blog""
+
+exit 0
